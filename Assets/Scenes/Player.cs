d52bb26139ccd6a05a1b96f44f73392e339a58ce
minfenglu <contact.minfeng@gmail.com>
2020-06-26 14:44:38 -0700

    first commit

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetKeyDown("space")){
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+                Debug.Log(hit.transform.name);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
