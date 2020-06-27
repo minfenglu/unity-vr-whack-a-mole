bb6e3f134ddbeb13becf526f9e81502a8c92727c
minfenglu <contact.minfeng@gmail.com>
2020-06-26 23:44:56 -0700

    make moles randomly rise

---
+++ Assets/Scenes/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject moleContainer;
+    public float spawnDuration = 3f;
+    public float minimumSpawnDuraiton = 1.5f;
+    public float spawnDecrement = 0.1f;
+    private float spawnTimer = 0f;
+    private Mole[] moles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moles = moleContainer.GetComponentsInChildren<Mole>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0) {
+            moles[Random.Range(0, moles.Length)].Rise();
+            spawnDuration -= spawnDecrement;
+            if (spawnDuration < minimumSpawnDuraiton) {
+                spawnDuration = minimumSpawnDuraiton;
+            }
+            spawnTimer = spawnDuration;
+        }
+    }
+}
--- Assets/Scenes/Mole.cs
+++ Assets/Scenes/Mole.cs
@@ -6,31 +6,51 @@ public class Mole : MonoBehaviour
 {
     public float visibleHeight = 0.2f;
     public float hiddenHeight = -0.3f;
-    public float speed = 1f;
-
+    public float speed = 4f;
+    public float disappearDuration = 2f;
+    
+    private float disappearTimer = 0f;
     private Vector3 targetPosition;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        targetPosition = new Vector3(
-            transform.localPosition.x,
-            visibleHeight,
-            transform.localPosition.z
-        );
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
-    }
-
-    public void OnHit() {
         targetPosition = new Vector3(
             transform.localPosition.x,
             hiddenHeight,
             transform.localPosition.z
         );
+        transform.localPosition = targetPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disappearTimer -= Time.deltaTime;
+        if (disappearTimer <= 0) {
+            Hide();
+        }
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+    }
+
+    public void Rise() {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            visibleHeight,
+            transform.localPosition.z
+        );
+        disappearTimer = disappearDuration;
+    }
+
+    public void Hide() {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            hiddenHeight,
+            transform.localPosition.z
+        );
+    }
+
+    public void OnHit() {
+        Hide();
     }
 }
